dc3b164061dd4edc2fad0dc3f84335c2570d3495
nessa29 <vanessay10@nycstudents.net>
2024-09-09 03:18:00 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("my name is vanessa");
+        System.out.println();
+        System.out.println("hi");
+        System.out.print("Bye");
+    }
+}
